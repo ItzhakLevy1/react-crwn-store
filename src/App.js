cb830a10405d01485c909f366d64b091de65d070
איציק לוי <itzhaklevydev@gmail.com>
2022-06-01 22:41:08 +0300

    created project with create reace app and added files for homepage

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
-import React, { Component } from "react";
-import { CardList } from "./components/card-list/card-list.component";
-import { SearchBox } from "./components/search-box/search-box.components";
-import './App.css';
+import React from "react";
+import HomePage from "./homepage.component";
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      monsters: [], 
-      searchField: ""
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(users => this.setState({ monsters:users }));
-    // this.focus();
-  }
-
-  handleChange = e => {
-    this.setState({ searchField: e.target.value });
-  };
-
-  render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
-      ); 
-
-    return (
-      <div className="App">
-        <h1> Monsters Rolodex </h1>
-          <SearchBox
-          autoFocus
-          placeholder="search monsters"
-          handleChange={e => this.setState({ searchField:e.target.value })}
-          />
-        <CardList monsters={filteredMonsters}/>
+function App() {
+  return <div> 
+    <HomePage />
     </div>
-    );
-  }
 }
 
 export default App;
