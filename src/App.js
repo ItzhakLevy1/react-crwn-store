8e83368ef0b73ccd267325fb2c4834e1de99060e
איציק לוי <itzhaklevydev@gmail.com>
2022-06-12 13:13:36 +0300

    introducing reactrouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
-  return <div> 
-    <HomePage />
+  return (
+  <div> 
+    <Routes>
+    <Route path='/react-crwn-store' element={<HomePage/>} /> 
+    {/* Exactly (true or false) when the path is "/", "component={HomePage}" is the component we want to render */}
+    <Route path="/hats" component={HatsPage} />
+    </Routes>
     </div>
+  );
 }
 
 export default App;
